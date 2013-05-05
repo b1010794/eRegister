ff6488d44bca462c7a894bff5999dadf699bb991
b1010794 <edgunn77@gmail.com>
2013-05-05 16:53:11 +0100

    Began to edit bootstrap, set up one entry for instructor

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,24 @@
+import uk.ac.shu.webarch.eregister.*;
+
 class BootStrap {
 
     def init = { servletContext ->
+      
+    println("BootStrap::init");
+    def ed_instructor = Instructor.findByStaffId("777777e");
+
+  if(ed_instructor == null){
+    println("Instructor not found, create..");
+    ed_instructor = new Instructor();
+    ed_instructor.staffId = "777777e";
+    ed_instructor.instructorName = "Ed Gunn";
+    ed_instructor.save();
     }
+
+    else{
+    println("Result of find by staffId for ed: ${ed_instructor}");
+    }
+  }
     def destroy = {
     }
 }
